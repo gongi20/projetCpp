89f7fc7fc0b625b0410b1152909a0962785a977a
gongi <mehdi.gongi@esprit.tn>
2023-03-20 20:07:01 +0100

    First commit main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "mainwindow.h"
+#include "connexion.h"
+#include <QApplication>
+#include <QMessageBox>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Connection c;
+    bool test=c.createconnect();
+    MainWindow w;
+    if(test)
+    {
+         w.show();
+         QMessageBox::information(nullptr, QObject::tr("database is open"),
+          QObject::tr("coonection successful.\n" "click cancel to exsit"),QMessageBox::Cancel);
+
+    }
+    else
+    {
+         QMessageBox::critical(nullptr, QObject::tr("database is not open"),
+                               QObject::tr("coonection faild .\n" "click cancel to exsit"),QMessageBox::Cancel);
+    }
+
+
+
+    return a.exec();
+}
